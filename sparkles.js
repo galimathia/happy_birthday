7914eca742d86f13b018d890274aeaa6bf4ea592
Savva <persp29@yandex.ru>
2026-07-25 18:37:37 +0300

    changed sparkles

--- sparkles.js
+++ sparkles.js
@@ -15,13 +15,15 @@
     const left = random(0, 100);
     const duration = random(4, 9); // seconds
     const color = colors[Math.floor(Math.random() * colors.length)];
-    const drift = Math.round(random(-80, 80));
+    const drift1 = Math.round(random(-80, 80));
+    const drift2 = Math.round(random(-80, 80));
     const top = random(10, 15);
 
     el.style.setProperty('--size', '10px');
     el.style.setProperty('--duration', duration + 's');
     el.style.setProperty('--color', color);
-    el.style.setProperty('--drift', drift + 'px');
+    el.style.setProperty('--driftbefore', drift1 + 'px');
+    el.style.setProperty('--driftafter', drift2 + 'px');
     el.style.setProperty('--top', top + 'vh');
 
     el.style.left = left + '%';
